49f4109bc5caebdd057eb2723b2c716d05b5c5fb
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-04-13 23:27:03 +0530

    c is running

--- hello.c
+++ hello.c
@@ -1,12 +1,14 @@
-
-
 #include <stdio.h>
-
+// #include "cs50.h"
 
 int main(void)
 {
 
+    for(int i =0;i<4;i++){
+  
+        printf("meow\n");
+    }
 
     // string answer = get_string("Waht is your name? ");
-    // printf("hello, %s\n", answer);
+    // printf("hello, %s", answer);
 }

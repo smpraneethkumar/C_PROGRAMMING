4b8631046ed042c8e1e724da6b8a617a3c7e18ee
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-04-17 23:45:29 +0530

    conditionals

---
+++ conditionals.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main(void){
+
+
+    if(2 > 5){
+        printf("is ture\n");
+    }else{
+        printf("is false\n");
+    }
+
+
+    if(4 == 4){
+        printf(" YES");
+    }
+}
--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main(void)
 {
 
     for(int i =0;i<4;i++){
-  
+    
         printf("meow\n");
     }
 
